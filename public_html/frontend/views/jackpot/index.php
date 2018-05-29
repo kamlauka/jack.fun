d0b02a2188a28d1486f74f5a2a5499819b09d4d9
sd <sd@terlabs.com>
2018-05-30 00:49:21 +0300

    fix page jackpot

--- public_html/frontend/views/jackpot/index.php
+++ public_html/frontend/views/jackpot/index.php
@@ -1,1 +1,5 @@
-Джекпот
+<?php if($model){?>
+<h2>Jackpot № <?php echo $model->id ?></h2><br>
+<h4>Sum of the jackpot: <?php echo $model->total ?> ETH</h4><br>
+<h4>Date: <?php echo $model->date_start ?></h4><br>
+<?php }else{echo 'Активных джекпотов не найдено';}?>
