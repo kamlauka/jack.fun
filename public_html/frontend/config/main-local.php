4c385d8c08c7fd1a5badc4bb5d55113eb5e37a0f
sd <sd@terlabs.com>
2018-05-29 23:45:39 +0300

    fix bug config

---
+++ public_html/frontend/config/main-local.php
@@ -0,0 +1,26 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'fef1bcbc7a6964015a1c2a6a1f6a0f78',
+        ],
+    ],
+];
+
+
+// configuration adjustments for 'dev' environment
+$config['bootstrap'][] = 'debug';
+$config['modules']['debug'] = [
+    'class' => 'yii\debug\Module',
+];
+
+$config['bootstrap'][] = 'gii';
+$config['modules']['gii'] = [
+    'class' => 'yii\gii\Module',
+    'allowedIPs' => ['*'],
+];
+
+
+return $config;
