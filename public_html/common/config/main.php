174129d97f814523eac2b0eb3d4ddbdc5fe1dade
NastyaSuvorova <as@terlabs.com>
2018-06-26 15:28:58 +0300

    fixed bugs?

---
+++ public_html/common/config/main.php
@@ -0,0 +1,28 @@
+<?php
+return [
+    'aliases' => [
+        '@bower' => '@vendor/bower-asset',
+        '@npm'   => '@vendor/npm-asset',
+    ],
+    'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+    'components' => [
+        'cache' => [
+            'class' => 'yii\caching\FileCache',
+        ],
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=admin_jackpot',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8',
+            'enableSchemaCache' => true,
+            'schemaCacheDuration' => 3600,
+            'schemaCache' => 'cache',
+        ],
+
+
+
+
+
+    ],
+];
