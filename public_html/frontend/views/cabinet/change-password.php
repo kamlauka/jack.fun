cf41539793b6e0b789ffdbf5639f318ed4539f14
NastyaSuvorova <as@terlabs.com>
2018-07-05 12:28:10 +0300

    hot fixed bugs (cabinet editing some and more)

--- public_html/frontend/views/cabinet/change-password.php
+++ public_html/frontend/views/cabinet/change-password.php
@@ -4,19 +4,25 @@ use yii\helpers\Html;
 /**
  * @var $model \frontend\models\ChangePasswordForm
  */
-$this->title = 'change passwod';
+$this->title = 'change password';
 $form = ActiveForm::begin([
     'action' => \yii\helpers\Url::toRoute(['change-password']),
 
 ]);
 ?>
+<div class="user-form">
+    <div class="user-form__editing-container">
 <h1><?= Html::encode($this->title) ?></h1>
 <div class="col-sm-6 col-sm-offset-3">
-    <?= $form->field($model, 'passold')->textInput(['class'=>'label__input input-text'])->label('Old password'); ?>
-    <?= $form->field($model, 'password')->textInput(['class'=>'label__input input-text'])->label('New password'); ?>
-    <?= $form->field($model, 'password_repeat')->textInput(['class'=>'label__input input-text'])->label('Repeat new password');; ?>
+    <?= $form->field($model, 'passold')->textInput(['class'=>'label__input input-text'])->label('Old password',['class'=>'about-user-info__title']); ?>
+    <?= $form->field($model, 'password')->textInput(['class'=>'label__input input-text'])->label('New password',['class'=>'about-user-info__title']); ?>
+    <?= $form->field($model, 'password_repeat')->textInput(['class'=>'label__input input-text'])->label('Repeat new password',['class'=>'about-user-info__title']);; ?>
+    <br>
     <div class="form-group">
         <?= \yii\helpers\Html::submitButton('Save', ['class' => 'button button_gold']) ?>
+    </div>
+
+</div>
     </div>
 
 </div>
--- public_html/frontend/views/cabinet/editing.php
+++ public_html/frontend/views/cabinet/editing.php
@@ -7,7 +7,7 @@ use yii\widgets\ActiveForm;
 /* @var $this yii\web\View */
 /* @var $model common\models\User */
 
-$this->title = 'Editing';
+//$this->title = 'Editing';
 
 ?>
 <div class="user-update">
@@ -20,16 +20,46 @@ $this->title = 'Editing';
                 'action' => \yii\helpers\Url::toRoute(['editing', 'id' => $model->id]),
             ]); ?>
 
-            <?= $form->field($model, 'avatar')->fileInput() ?>
+
+
+
+
+            <div class="user-info about-user-info__paragraph" >
+
+                <div class="user-logo-wrap">
+                    <div class="user-logo-container">
+                        <?php if(isset($model->avatar)){
+                            //если есть картинка
+                            echo Html::img($user->avatar,['alt'=>"your logo", 'class'=>"user-logo about-user-info__user-logo"]);
+                        } else{
+                            //если нет картинки
+                            echo '<img class="user-logo about-user-info__user-logo" src="../../images/cabinet/default-logo.png" alt="">';
+                        }?>
+                        <?= $form->field($model, 'avatar')->fileInput() ?>
+                    </div>
+                </div>
+
+
+                <?php
+                if(isset($user->username)){
+                    echo Html::tag('p',$user->username,['class'=>'user-name about-user-info__user-name']);
+                } ?>
+
+            </div>
+
+
+
+
+
             <br>
-            <?= $form->field($model, 'username')->textInput(['maxlength' => true, 'class'=>'label__input input-text']) ?>
+            <?= $form->field($model, 'phone')->textInput(['maxlength' => true, 'class'=>'label__input input-text'])->label('my phone-number:',['class'=>'about-user-info__title']); ?>
+            <?= $form->field($model, 'wallet')->textInput(['maxlength' => true, 'class'=>'label__input input-text'])->label('my wallet:',['class'=>'about-user-info__title']); ?>
+            <?= $form->field($model, 'email')->textInput(['maxlength' => true, 'class'=>'label__input input-text'])->label('my email:',['class'=>'about-user-info__title']); ?>
 
-            <?= $form->field($model, 'email')->textInput(['maxlength' => true, 'class'=>'label__input input-text']) ?>
 
-            <?= $form->field($model, 'wallet')->textInput(['maxlength' => true, 'class'=>'label__input input-text']) ?>
 
-            <?= $form->field($model, 'phone')->textInput(['maxlength' => true, 'class'=>'label__input input-text']) ?>
 
+<br>
             <div class="form-group">
                 <?= Html::submitButton('Save', ['class' => 'button button_gold']) ?>
             </div>
