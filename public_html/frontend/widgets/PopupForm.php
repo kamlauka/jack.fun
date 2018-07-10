3671bb6443d90e2166897b1a64312fd7b0d9d53a
dzystas <sd@terlabs.com>
2018-07-10 15:28:33 +0300

    fix

--- public_html/frontend/widgets/PopupForm.php
+++ public_html/frontend/widgets/PopupForm.php
@@ -23,7 +23,7 @@ class PopupForm extends Widget {
             parent::init();
             echo $this->render('popup-form/'.$this->view, [
                 'model' => $this->model,
-                'lottery' => $lottery['data'],
+                'lottery' => $lottery,
                 'wallet' => $wallet,
             ]);
 
