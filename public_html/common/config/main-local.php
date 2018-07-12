426da4159c74fbe4db3783ad989c34e07c0f78d8
dzystas <sd@terlabs.com>
2018-07-12 11:17:05 +0300

    fix

--- public_html/common/config/main-local.php
+++ public_html/common/config/main-local.php
@@ -7,6 +7,14 @@ return [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
             'useFileTransport' => false,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'host' => 'smtp.gmail.com',
+                'username' => 'username@gmail.com',
+                'password' => 'password',
+                'port' => '587',
+                'encryption' => 'tls',
+            ],
         ],
     ],
 ];
