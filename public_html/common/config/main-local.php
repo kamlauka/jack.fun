b1b9c1457a8c3884467cdd0cefa5208e867bee54
kamlauka <kamlauka@gmail.com>
2018-07-26 12:35:44 +0300

    merge

--- public_html/common/config/main-local.php
+++ public_html/common/config/main-local.php
@@ -6,14 +6,11 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
-<<<<<<< HEAD
 
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
 
-=======
->>>>>>> cf3be5fd93b9153a392bda03cb2635f263211465
             'useFileTransport' => false,
         ],
     ],
