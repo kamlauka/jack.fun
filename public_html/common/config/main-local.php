cf3be5fd93b9153a392bda03cb2635f263211465
dzystas <sd@terlabs.com>
2018-07-26 10:58:19 +0300

    marging

--- public_html/common/config/main-local.php
+++ public_html/common/config/main-local.php
@@ -6,12 +6,6 @@ return [
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
             'viewPath' => '@common/mail',
-<<<<<<< HEAD
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-=======
->>>>>>> cc03a62d66436ca5fcc24a44b214fdbcf2fe9d95
             'useFileTransport' => false,
         ],
     ],
