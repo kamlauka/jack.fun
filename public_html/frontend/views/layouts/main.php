a0dd67ad0d7c28fb05fec682fde3fbf8e489b45c
dzystas <sd@terlabs.com>
2018-07-31 14:45:46 +0300

    refactoring

--- public_html/frontend/views/layouts/main.php
+++ public_html/frontend/views/layouts/main.php
@@ -82,7 +82,7 @@ AppAsset::register($this);
 
     <div class="orange-border orange-border_edge"></div>
 
-    <div  onclick="targetFunc(event)" class="forms <?= isset(Yii::$app->params['popup']) ? 'forms_active-flex': ''?>" data="<?= isset(Yii::$app->params['popup']) ? 'activ': ''?>">
+    <div   <!--   зачем этот блок если он не работает? -->  onclick="targetFunc(event)" class="forms <?= isset(Yii::$app->params['popup']) ? 'forms_active-flex': ''?>" data="<?= isset(Yii::$app->params['popup']) ? 'activ': ''?>">
 
         <div class="popup forms__popup" >
 
