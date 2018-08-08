e6e2309142874f3d71aaa73c89e4a8e9cc970243
NastyaSuvorova <as@terlabs.com>
2018-08-08 10:44:26 +0300

    front optimizied clouds

--- public_html/frontend/web/js/animateClouds.js
+++ public_html/frontend/web/js/animateClouds.js
@@ -18,53 +18,106 @@
 //
 //
 
+
+
+
+
 var cloud = new Image();
-var canvases = document.getElementsByClassName('canvas');
-var ts0;
-var canvasSize = document.documentElement.clientWidth;
+cloud.src = "http://i.imgur.com/GigS3KR.png";
 
+// размеры облака
+var imageWidth = 800;
+var imageHeight = 459;
 
-for(let i = 0; i < canvases.length; i++) {
-    let ctx = canvases[i].getContext('2d');
-    function draw(ts) {
-        ctx.clearRect(0,0,canvasSize,canvasSize);
+// делаем размер canvas больше размера изображения,
+// т.к. анимация выводит изображение за свои рамки
+var canvasWidth = imageWidth + 100;
+var canvasHeight = imageHeight + 100;
 
-        ctx.save();
-        // cloud
-        if(!ts0) ts0 = ts;
-        const dt = ts - ts0; // milliseconds
+function draw(ctx, dt) {
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    ctx.save();
 
-        ctx.translate(50, 50 + Math.sin(dt/500)/20);
-        ctx.rotate( Math.sin(dt/340) / 200 );
+    ctx.translate(50, 50 + Math.sin(dt / 500) / 20);
+    ctx.rotate( Math.sin(dt/340) / 200 );
 
-        ctx.drawImage(cloud,0,0, canvasSize*0.95, canvasSize*0.97);
-        ctx.restore();
-        ctx.save();
-        ctx.translate(5, 0 + Math.sin(dt/382)*2);
-        ctx.rotate( Math.sin(dt/630) / 40 );
-        // ctx.drawImage(cloud2,150,100,canvasSize*0.84, canvasSize*0.84);
-        // ctx.restore();
+    ctx.drawImage(cloud, 0, 0, imageWidth, imageHeight,  // source rectangle
+        0, 0, imageWidth, imageHeight); // destination rectangle
 
-        // ctx.save();
-        // ctx.translate(8, 150 + Math.sin(dt/198)*3);
-        // ctx.rotate( Math.sin(dt/500) / 450 );
-        // ctx.drawImage(cloud3,0,0, canvasSize*0.7, canvasSize*0.7);
-        ctx.restore();
-        ctx.restore();
-        window.requestAnimationFrame(draw);
-    }
+    ctx.restore();
+    // ctx.save();
+    // ctx.translate(5, 0 + Math.sin(dt / 382) * 2);
+    // ctx.rotate(Math.sin(dt / 630) / 40);
+    ctx.restore();
 
-    function init(){
-        cloud.src = '../../../images/common/cloud.png';
-        // cloud2.src= 'http://i.imgur.com/y3JAe69.png';
-        // cloud3.src= 'http://i.imgur.com/v30JIWp.png';
-        canvases[i].setAttribute('width', canvasSize);
-        canvases[i].setAttribute('height', canvasSize);
-        window.requestAnimationFrame(draw);
-    }
-
-    init();
+    window.requestAnimationFrame(function(dt) {
+        draw(ctx, dt);
+    });
 }
+
+$("canvas").each(function(i, canvas) {
+    var ctx = canvas.getContext("2d"); // не нужно получать context в каждом цикле анимации
+    ctx.canvas.width  = canvasWidth;
+    ctx.canvas.height = canvasHeight;
+
+    window.requestAnimationFrame(function(dt) {
+        draw(ctx, dt);
+    });
+});
+
+
+
+
+
+
+
+// var cloud = new Image();
+// var canvases = document.getElementsByClassName('canvas');
+// var ts0;
+// var canvasSize = document.documentElement.clientWidth;
+//
+//
+// for(let i = 0; i < canvases.length; i++) {
+//     let ctx = canvases[i].getContext('2d');
+//     function draw(ts) {
+//         ctx.clearRect(0,0,canvasSize,canvasSize);
+//
+//         ctx.save();
+//         // cloud
+//         if(!ts0) ts0 = ts;
+//         const dt = ts - ts0; // milliseconds
+//
+//         ctx.translate(50, 50 + Math.sin(dt/500)/20);
+//         ctx.rotate( Math.sin(dt/340) / 200 );
+//
+//         ctx.drawImage(cloud,0,0, canvasSize*0.95, canvasSize*0.97);
+//         ctx.restore();
+//         ctx.save();
+//         ctx.translate(5, 0 + Math.sin(dt/382)*2);
+//         ctx.rotate( Math.sin(dt/630) / 40 );
+//         // ctx.drawImage(cloud2,150,100,canvasSize*0.84, canvasSize*0.84);
+//         // ctx.restore();
+//
+//         // ctx.save();
+//         // ctx.translate(8, 150 + Math.sin(dt/198)*3);
+//         // ctx.rotate( Math.sin(dt/500) / 450 );
+//         // ctx.drawImage(cloud3,0,0, canvasSize*0.7, canvasSize*0.7);
+//         ctx.restore();
+//         ctx.restore();
+//         window.requestAnimationFrame(draw);
+//     }
+//
+//     function init(){
+//         cloud.src = '../../../images/common/cloud.png';
+//         // cloud2.src= 'http://i.imgur.com/y3JAe69.png';
+//         // cloud3.src= 'http://i.imgur.com/v30JIWp.png';
+//         canvases[i].setAttribute('width', canvasSize);
+//         canvases[i].setAttribute('height', canvasSize);
+//         window.requestAnimationFrame(draw);
+//     }
+//
+//     init();
+// }
 
 //
 //
