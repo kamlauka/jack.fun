32ec8a5f7d11ce5a4af6d12618793470e490b914
NastyaSuvorova <as@terlabs.com>
2018-08-08 10:48:40 +0300

    merge

--- public_html/frontend/web/js/animateClouds.js
+++ public_html/frontend/web/js/animateClouds.js
@@ -18,78 +18,12 @@
 //
 //
 // 'use strict'
-<<<<<<< HEAD
+
 // var canvases = document.getElementsByClassName('canvas');
-var cloud = new Image();
-var ctx = [];
-var ts0;
-var canvasSize = document.documentElement.clientWidth;
-var time, dt;
-
-function timeForAnimation() {
-    time = new Date();
-    if(!ts0) ts0 = time;
-    dt = time - ts0; // milliseconds
-}
-
-
-$('canvas').each(function(i,elem) {
-    ctx[i] = elem.getContext('2d');
-    function draw() {
-
-
-        ctx[i].clearRect(0,0,canvasSize,canvasSize);
-        ctx[i].save();
-        // cloud
-
-
-
-        timeForAnimation();
-        ctx[i].translate(50, 50 + Math.sin(dt/500)/20);
-        ctx[i].rotate( Math.sin(dt/340) / 200 );
-
-        ctx[i].drawImage(cloud,0,0, canvasSize*0.95, canvasSize*0.97);
-        ctx[i].restore();
-        ctx[i].save();
-        ctx[i].translate(5, 0 + Math.sin(dt/382)*2);
-        ctx[i].rotate( Math.sin(dt/630) / 40 );
-        // ctx.drawImage(cloud2,150,100,canvasSize*0.84, canvasSize*0.84);
-        // ctx.restore();
-
-        // ctx.save();
-        // ctx.translate(8, 150 + Math.sin(dt/198)*3);
-        // ctx.rotate( Math.sin(dt/500) / 450 );
-        // ctx.drawImage(cloud3,0,0, canvasSize*0.7, canvasSize*0.7);
-        ctx[i].restore();
-        ctx[i].restore();
-        window.requestAnimationFrame(draw);
-
-    }
-    function init(elem){
-
-        var t0 = performance.now();
-
-        cloud.src = '../../../images/common/cloud.png';
-        // cloud2.src= 'http://i.imgur.com/y3JAe69.png';
-        // cloud3.src= 'http://i.imgur.com/v30JIWp.png';
-        elem.setAttribute('width', canvasSize);
-        elem.setAttribute('height', canvasSize);
-        window.requestAnimationFrame(draw);
-        var t1 = performance.now();
-        console.log("Call to doSomething took " + (t1 - t0) + " milliseconds.");
-    }
-
-
-    init(elem);
-=======
-
-
 
 
 
 var cloud = new Image();
->>>>>>> as
-
 cloud.src = "http://i.imgur.com/GigS3KR.png";
 
 // размеры облака
